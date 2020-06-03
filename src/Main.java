8e18ae67378c128f8746172dff543406160757e3
Wojciech Kos <obohp@wp.pl>
2020-06-03 19:21:32 +0200

    changed Main added adding

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Uczę\nsię\nprogramowania");
         System.out.println("i\ncommitowania");
         System.out.println("A teraz operacje arytmetyczne:");
+        System.out.println("2 + 2 = " + 2 + 2);
     }
 }
