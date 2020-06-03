33ccd627e2648ad7f0a2b3e078a96142f0ad044e
Wojciech Kos <obohp@wp.pl>
2020-06-03 19:19:42 +0200

    changed Main added arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Uczę\nsię\nprogramowania");
         System.out.println("i\ncommitowania");
+        System.out.println("A teraz operacje arytmetyczne:");
     }
 }
